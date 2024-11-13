5d3f0b3a89c2c50719eacbe4118dca331bd2829b
Lucas Pedroza <40873230+pnwpedro@users.noreply.github.com>
2024-11-13 19:03:46 +0100

    Add tests for Customer endpoint (#9)
    * Add unit test project
    
    * Finish Customer tests
    
    * Push schema for tests
    
    * Fix setup

---
+++ DotNetSampleApp.Tests/TestSetup.cs
@@ -0,0 +1,38 @@
+using DotNetSampleApp.Services;
+using Fauna;
+using NUnit.Framework;
+
+namespace DotNetSampleApp.Tests;
+
+[SetUpFixture]
+public class TestSetup
+{
+    public static string Secret = "";
+    
+    [OneTimeSetUp]
+    public void SetupOnce()
+    {
+        if (Environment.GetEnvironmentVariable("FAUNA_SECRET") == null)
+        {
+            var c = new Client(new Configuration("secret")
+            {
+                Endpoint = new Uri("http://localhost:8443"),
+            });
+            Secret = c.QueryAsync<string>(Query.FQL($$"""
+                                                      let k = Key.create({role: 'admin', database: 'ECommerceDotnet'})
+                                                      k.secret
+                                                      """)).Result.Data;
+        }
+        else
+        {
+            Secret = Environment.GetEnvironmentVariable("FAUNA_SECRET")!;
+        }
+        
+        var client = new Client(new Configuration(Secret)
+        {
+            Endpoint = new Uri("http://localhost:8443"),
+        });
+        SeedService.Init(client);
+    }
+
+}
